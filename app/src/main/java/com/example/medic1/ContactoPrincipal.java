c131bad8057818a8e94c8901fdc95eb45fff884d
DoraPulido2020 <dpulidop2@ucentral.edu.co>
2023-11-27 15:28:54 -0500

    Adelanto Vistas

---
+++ app/src/main/java/com/example/medic1/ContactoPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.medic1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactoPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacto_principal);
+    }
+}
